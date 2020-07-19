10813bd87c6818c305b95a61fe6c2a98359948c3
Caleb Habtegebriel <calebhabtegebriel@calebs-air.home>
2020-07-19 01:41:07 -0500

    Updates

---
+++ precache-manifest.5283ca1b6898159d3f9e44299029500f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cd2c25c65423e64c2fcb08fb1c618a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9ac36930d456e91a74",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "8c52439d33a3e3b55640",
+    "url": "/static/css/main.1010ed8d.chunk.css"
+  },
+  {
+    "revision": "7d9ac36930d456e91a74",
+    "url": "/static/js/2.1f99a6e5.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.1f99a6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c52439d33a3e3b55640",
+    "url": "/static/js/main.51afa938.chunk.js"
+  },
+  {
+    "revision": "5eeb8d0fd802ead6c0b5",
+    "url": "/static/js/runtime-main.aa4df6ff.js"
+  },
+  {
+    "revision": "97381e30e37b8c10fd29e66271d6ca05",
+    "url": "/static/media/Avatar-Maker.97381e30.png"
+  }
+]);
